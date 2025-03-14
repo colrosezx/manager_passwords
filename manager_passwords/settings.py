453772c2651e1342399ff6b9358fe03b6858b6f3
colrosezx <kirillkovalev-2005@mail.ru>
2025-03-14 22:52:53 +0500

    add docker + docket-compose with config file

--- manager_passwords/settings.py
+++ manager_passwords/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'manager_passwords.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'passwords_db',
-        'USER': 'postgres',
-        'PASSWORD': '197386245',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", "db"),
+        'PORT': config("DB_PORT"),
     }
 }
 
